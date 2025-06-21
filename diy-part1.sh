5d90f6c230c2dc4acc6d7f3bf99ecebe4295a0c5
wans10 <51012039+wans10@users.noreply.github.com>
2025-06-21 23:58:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+cd openwrt
+git pull origin master
+./scripts/feeds update -a
+./scripts/feeds install -a
