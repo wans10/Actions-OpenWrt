327e6a36ed64530d8274aa208a79376e22c9146c
wans10 <51012039+wans10@users.noreply.github.com>
2025-06-22 14:24:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,22 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字
+sed -i 's/OpenWrt/RaspberryPi-4B/g' package/base-files/files/bin/config_generate
+
+# 修改默认wifi名称ssid为OpenWrt-5G
+sed -i 's/ssid=OpenWrt/ssid=OpenWrt-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认wifi密码key为Yang!984
+sed -i 's/key=$/key=Yang!984/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改默认wifi加密为psk2
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 设置密码为Yang!984
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
